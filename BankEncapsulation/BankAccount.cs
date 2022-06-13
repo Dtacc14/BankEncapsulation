1126e78d01516ae717fb2e293cddef7981befd87
David Taccetta <taccetta14@gmail.com>
2022-06-13 15:06:55 -0400

    Initial BankEncapsulation Project Commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public double Deposit(double x)
+        {
+            return _balance += x;
+        }
+
+        public double Withdrawl(double x)
+        {
+            return _balance -= x;
+        }
+
+        public string GetBalance()
+        {
+            return $"You have ${_balance} in your account.";
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bank1 = new BankAccount();
+            Console.WriteLine(bank1.GetBalance());
+
+            bank1.Deposit(100);
+            Console.WriteLine(bank1.GetBalance());
+
+            bank1.Withdrawl(50);
+            Console.WriteLine(bank1.GetBalance());
+            
+        }
+    }
+}
